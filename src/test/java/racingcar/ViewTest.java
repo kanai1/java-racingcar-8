2dfce380d07b83574b0f4d023d91928b981e03f7
kanai1 <ksh30918@kakao.com>
2025-10-21 22:31:55 +0900

    Test(ViewTest): printWinnersTest 추가
    - 최종 우승자를 출력하는 메소드의 테스트코드 추가
    - 최종우승자가 1명인 경우와 2명(이상)인 경우를 각각 테스트
    - 각 테스트 메소드에 DisplayName 어노테이션을 달아서 결과를 확인하기 편하게 수정

--- src/test/java/racingcar/ViewTest.java
+++ src/test/java/racingcar/ViewTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,6 +29,7 @@ class ViewTest {
     }
 
     @Test
+    @DisplayName("실행 결과를 출력하는 메소드 테스트")
     void printResultOfRaceTest() {
         View testView = new View();
         List<Car> testList = new ArrayList<Car>();
@@ -38,5 +40,26 @@ class ViewTest {
         testList.get(1).addScore();
         testView.printResultOfRace(testList);
         assertThat(outputStreamCaptor.toString().trim()).contains("실행 결과", "test1 : --", "test2 : -");
+    }
+
+    @Test
+    @DisplayName("최종 우승자가 다수인 경우를 출력하는 테스트")
+    void printWinnersTest1() {
+        View testView = new View();
+        List<Car> testList = new ArrayList<Car>();
+        testList.add(new Car("test1"));
+        testList.add(new Car("test2"));
+        testView.printWinners(testList);
+        assertEquals("최종 우승자 : test1, test2", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    @DisplayName("최종 우승자가 1명인 경우를 출력하는 테스트")
+    void printWinnersTest2() {
+        View testView = new View();
+        List<Car> testList = new ArrayList<Car>();
+        testList.add(new Car("test1"));
+        testView.printWinners(testList);
+        assertEquals("최종 우승자 : test1", outputStreamCaptor.toString().trim());
     }
 }
