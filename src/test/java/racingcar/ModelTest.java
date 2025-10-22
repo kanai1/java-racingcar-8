e6e7262cf8e206591e9b4b23fbab13d212a53a0a
kanai1 <ksh30918@kakao.com>
2025-10-22 21:34:21 +0900

    test(ModelTest): 최종 우승자 테스트 추가
    - 최종 우승자가 1명일 경우와 2명인 경우를 ParameterizedTest로 테스트

--- src/test/java/racingcar/ModelTest.java
+++ src/test/java/racingcar/ModelTest.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatList;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 import camp.nextstep.edu.missionutils.test.Assertions;
@@ -27,7 +29,14 @@ public class ModelTest {
         );
     }
 
-    @ParameterizedTest(name = "{index} {displayName}({arguments})")
+    static Stream<Arguments> argumentsForTestGetWinner() {
+        return Stream.of(
+                Arguments.of(2, 1, List.of("test1")),
+                Arguments.of(2, 2, List.of("test1", "test2"))
+        );
+    }
+
+    @ParameterizedTest(name = "{displayName}({arguments})")
     @DisplayName("경주 실행 테스트")
     @MethodSource("argumentsForTestProgressRace")
     public void progressRaceTest(int move1, int move2) {
@@ -52,6 +61,21 @@ public class ModelTest {
                 },
                 move1, move2
         );
+    }
 
+    @ParameterizedTest(name = "{displayName}(score = {0}:{1}, expected = {2})")
+    @DisplayName("최종 우승자 테스트")
+    @MethodSource("argumentsForTestGetWinner")
+    public void getWinnerTest(int score1, int score2, List<String> expected) {
+        Model testModel = new Model();
+        List<String> testNames = List.of("test1", "test2");
+        testModel.setNameOfParticipants(testNames);
+        for (int i = 0; i < score1; i++) {
+            testModel.getParticipants().get(0).addScore();
+        }
+        for (int i = 0; i < score2; i++) {
+            testModel.getParticipants().get(1).addScore();
+        }
+        assertThatList(testModel.getWinner()).isEqualTo(expected);
     }
 }
