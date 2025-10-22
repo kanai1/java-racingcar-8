63c5950dc1b09c3860c9aa531b36ab23a977029f
kanai1 <ksh30918@kakao.com>
2025-10-23 01:52:54 +0900

    feat(Controller): verifyName 메소드 추가
    - 입력받은 이름을 검증하기 위한 메소드 추가
    - 이름의 개수, 길이, 공백 포함 여부, 중복 여부를 검사해서 boolean형태로 반환

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class Controller {
 
     private Model model;
@@ -8,5 +11,30 @@ public class Controller {
     public Controller() {
         model = new Model();
         view = new View();
+    }
+
+    public void initGame() {
+
+    }
+
+    public boolean verifyName(List<String> names) {
+        if(new HashSet<>(names).size() != names.size()) {
+            return false;
+        }
+        if (names.size() < 2 || names.size() > 10) {
+            return false;
+        }
+        for(String name: names) {
+            if (name.contains(" ")) {
+                return false;
+            }
+            if (name.isEmpty() || name.length() > 5) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void run() {
     }
 }
